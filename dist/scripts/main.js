e84544f23371072e4c59b2881f002e716412d9ac
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-06-08 18:22:55 -0500

    finished model

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,34 @@
 var user = new UserModel();
-var App = Backbone.Router.extend({
-	routes: {
-		'': 'profile',
-		'edit': 'edit'
-	},
-	profile: function() {
-		$('.page').hide();
-		$('#profile').show();
-	},
-	edit: function() {
-		$('.page').hide();
-		$('#edit').show();
-	}
-});
+$(document).ready(function() {
+	var App = Backbone.Router.extend({
+		routes: {
+			'': 'profile',
+			'edit': 'edit',
+			'home': 'profile'
+		},
+		profile: function() {
+			$('.page').hide();
+			$('#profile').show();
+		},
+		edit: function() {
+			$('.page').hide();
+			$('#edit').show();
+		}
+	});
 
 var app = new App();
 Backbone.history.start();
+
+	$('.form-horizontal').on('submit', function(e) {
+		console.log('user updated');
+		e.preventDefault();
+		user.set({
+			name: $('#name').val(),
+			email: $('#inputEmail3').val(),
+			role: $('#role').val(),
+			password: $('#inputPassword3').val()
+		});
+		console.log(user);
+	});
+	console.log(user);
+});
